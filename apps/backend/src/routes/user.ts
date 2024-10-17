fb0ea9cf694a9928bf263e8e6256539d12d76bc1
AayushGupta69 <aayushgupta69official@gmail.com>
2024-10-17 08:31:52 +0000

    Implemented route for user signup and added missing dependencies in database package

--- apps/backend/src/routes/user.ts
+++ apps/backend/src/routes/user.ts
@@ -1,9 +1,35 @@
 import { Router } from "express";
+import { userSignUpSchema } from "@e-commerce-platform/schemas";
+import prisma from "@e-commerce-platform/database/client";
+import bcrypt from "bcrypt";
 
 const router: Router = Router();
 
 router.post("/signup", async (req: any, res: any) => {
-  const { name, phone, email, password } = req.body;
+  const { success, data, error } = userSignUpSchema.safeParse(req.body);
+
+  if (!success) {
+    console.error(error);
+    return res.status(411).json({
+      message: "Invalid inputs. Please provide valid inputs.",
+    });
+  }
+
+  const password = await bcrypt.hash(data.password, 10);
+
+  const user = await prisma.user.create({
+    data: {
+      name: data.name,
+      phone: data.phone,
+      email: data.email,
+      password,
+    },
+  });
+
+  return res.status(201).json({
+    message: "Welcome! Your account has been created successfully!",
+    user,
+  });
 });
 
 export default router;
